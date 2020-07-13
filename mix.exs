15bc344557a8c86ea890ddef8aba4e5f85c0b014
Almir Sarajčić <almir.sarajcic@icloud.com>
2020-07-13 17:12:25 +0200

    Fix SSL connection error

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule PratiBa.MixProject do
   def application do
     [
       mod: {PratiBa.Application, []},
-      extra_applications: [:inets, :logger, :os_mon, :runtime_tools, :timex, :xmerl]
+      extra_applications: [:inets, :logger, :os_mon, :runtime_tools, :ssl, :timex, :xmerl]
     ]
   end
 
