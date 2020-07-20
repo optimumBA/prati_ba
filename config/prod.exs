24cc65720119973c40e846259d7bb61c19123f54
Almir Sarajčić <almir.sarajcic@icloud.com>
2020-07-20 07:28:44 +0200

    Output logs in JSON format

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,17 @@ config :prati_ba, PratiBaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger,
+  level: :info,
+  backends: [:console],
+  utc_log: true
+
+config :logger, :console,
+  utc_log: true,
+  format: {Timber.Formatter, :format},
+  metadata: :all
+
+config :prati_ba, PratiBa.Repo, log: false
 
 # ## SSL Support
 #
