6d59ee5ce9227eb389f538cefcdecf4c23eecb13
Almir Sarajčić <almir.sarajcic@icloud.com>
2020-11-22 08:48:40 +0100

    Switch to DO Spaces

--- config/releases.exs
+++ config/releases.exs
@@ -16,14 +16,7 @@ host =
   System.get_env("HOST") ||
     raise """
     environment variable HOST is missing.
-    For example: www.prati.ba
-    """
-
-asset_host =
-  System.get_env("ASSET_HOST") ||
-    raise """
-    environment variable ASSET_HOST is missing.
-    For example: static.prati.ba
+    For example: prati.ba
     """
 
 secret_key_base =
@@ -41,7 +34,7 @@ config :prati_ba, PratiBaWeb.Endpoint,
   ],
   secret_key_base: secret_key_base,
   url: [scheme: "https", host: host, port: 443],
-  static_url: [scheme: "https", host: asset_host, port: 443]
+  static_url: [scheme: "https", host: host, port: 443]
 
 admin_password =
   System.get_env("ADMIN_PASSWORD") ||
@@ -83,42 +76,49 @@ config :geolix,
     }
   ]
 
-aws_s3_bucket =
-  System.get_env("AWS_S3_BUCKET") ||
+asset_host =
+  System.get_env("ASSET_HOST") ||
     raise """
-    environment variable AWS_S3_BUCKET is missing.
+    environment variable ASSET_HOST is missing.
+    For example: static.prati.ba
+    """
+
+do_spaces_bucket =
+  System.get_env("DO_SPACES_BUCKET") ||
+    raise """
+    environment variable DO_SPACES_BUCKET is missing.
     For example: static.prati.ba
     """
 
 config :waffle,
   storage: Waffle.Storage.S3,
   storage_dir_prefix: "",
-  bucket: aws_s3_bucket,
+  bucket: do_spaces_bucket,
   asset_host: "https://#{asset_host}"
 
-aws_s3_id =
-  System.get_env("AWS_S3_ID") ||
+do_spaces_id =
+  System.get_env("DO_SPACES_ID") ||
     raise """
-    environment variable AWS_S3_ID is missing.
+    environment variable DO_SPACES_ID is missing.
     For example: AKIB2GHLQBL82IVL1B4N
     """
 
-aws_s3_secret =
-  System.get_env("AWS_S3_SECRET") ||
+do_spaces_secret =
+  System.get_env("DO_SPACES_SECRET") ||
     raise """
-    environment variable AWS_S3_SECRET is missing.
+    environment variable DO_SPACES_SECRET is missing.
     For example: 1qyPY8F93ZW4B2gHy1eR/U9BL2zqb3c0LB3CV4hV
     """
 
 config :ex_aws,
   json_codec: Jason,
-  access_key_id: aws_s3_id,
-  secret_access_key: aws_s3_secret,
-  region: "eu-central-1",
+  access_key_id: do_spaces_id,
+  secret_access_key: do_spaces_secret,
+  region: "fra1",
   s3: [
     scheme: "https://",
-    host: "s3.eu-central-1.amazonaws.com",
-    region: "eu-central-1"
+    host: "fra1.digitaloceanspaces.com",
+    region: "fra1"
   ]
 
 # ## Using releases (Elixir v1.9+)
