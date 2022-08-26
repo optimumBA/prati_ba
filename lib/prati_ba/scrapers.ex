3e57770e78e8fc0190631e62432c43925074d458
senaid123 <senaid968@gmail.com>
2022-08-26 18:08:28 +0200

    Scrapers implementation -Work on new scraper -add new scraper to scraper.ex

--- lib/prati_ba/scrapers.ex
+++ lib/prati_ba/scrapers.ex
@@ -25,7 +25,8 @@ defmodule PratiBa.Scrapers do
     SlobodnaBosnaScraper,
     SourceScraper,
     TheBosniaTimesScraper,
-    ZurnalScraper
+    ZurnalScraper,
+    StartBihScraper
   }
 
   @scrapers %{
@@ -47,7 +48,8 @@ defmodule PratiBa.Scrapers do
     "Slobodna Bosna" => SlobodnaBosnaScraper,
     "source.ba" => SourceScraper,
     "The Bosnia Times" => TheBosniaTimesScraper,
-    "Žurnal" => ZurnalScraper
+    "Žurnal" => ZurnalScraper,
+    "StartBiH" => StartBihScraper
   }
 
   @article_keys [:image, :original_id, :published_at, :title, :url]
@@ -64,6 +66,7 @@ defmodule PratiBa.Scrapers do
     case Map.get(scrapers, source_name) do
       nil -> nil
       scraper -> {source, scraper}
+      IO.puts scrapers
     end
   end
 
---
+++ lib/prati_ba/scrapers/start_bih_scraper.ex
@@ -0,0 +1,90 @@
+defmodule PratiBa.Scrapers.StartBihScraper do
+  @behaviour PratiBa.Scrapers.Scraper
+
+  @url "https://startbih.ba/"
+
+  alias PratiBa.Scrapers.ScrapingHelper
+
+  def articles(url \\ @url) do
+    response = ScrapingHelper.get(url)
+
+    with {:ok, %{status_code: 200, body: body}} <- response,
+         {:ok, html} <- Floki.parse_document(body) do
+      articles =
+        html
+        |> Floki.find(".row .mb-30 ul li h3 a")
+        |> Stream.map(&parse_article/1)
+
+      {:ok, articles}
+    else
+      {_, response} -> {:error, response}
+    end
+  end
+
+  def article_details(url) do
+    response = ScrapingHelper.get(url)
+
+    with {:ok, %{status_code: 200, body: body}} <- response,
+         {:ok, html} <- Floki.parse_document(body) do
+      article_content =
+        html
+        |> Floki.find(".row .mb-30")
+
+      date =
+        article_content
+        |> Floki.find("ul li span")
+        |> Floki.attribute("title")
+        |> Enum.at(0)
+        |> String.trim()
+
+      published_at =
+        date
+        |> Timex.parse!("{D}.{M}.{YYYY} u {h24}:{m}")
+        |> DateTime.from_naive!("Europe/Sarajevo")
+        |> DateTime.shift_zone!("Etc/UTC")
+        |> DateTime.to_naive()
+
+      # article = Map.put(article, :published_at, published_at);
+
+      image_url =
+        article_content
+        |> Floki.find("a .img-overlay-70 img")
+        |> Floki.attribute("data-src")
+        |> URI.encode()
+
+
+
+
+       # article = Map.put(article, :image_url, image_url)
+
+
+    end
+  end
+
+  defp parse_article(article) do
+    url =
+      article
+      |> Floki.attribute("href")
+      |> Enum.fetch!(1)
+
+    original_id =
+      url
+      |> String.split("/")
+      |> Enum.fetch!(4)
+
+    title =
+      article
+      |> Floki.text()
+      |> String.trim()
+
+    %{
+      original_id: original_id,
+      title: title,
+      description: nil,
+      published_at: nil,
+      author: nil,
+      image: nil,
+      url: url
+    }
+  end
+end
