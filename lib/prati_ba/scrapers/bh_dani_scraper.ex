45836b9249ceba6134e39fbfcfcf7de1f92f08e3
senaid123 <senaid968@gmail.com>
2022-08-31 17:52:19 +0200

    Implement scraper for bh-dani portal -add scraper for article -write functional tests for scraper

--- lib/prati_ba/scrapers/bh_dani_scraper.ex
+++ lib/prati_ba/scrapers/bh_dani_scraper.ex
@@ -9,9 +9,94 @@ defmodule PratiBa.Scrapers.BhDaniScraper do
     response = ScrapingHelper.get(url)
 
     with {:ok, %{status_code: 200, body: body}} <- response,
-    {:ok, html} <- Floki.parse_document(body) do
+         {:ok, html} <- Floki.parse_document(body) do
+      articles =
+        html
+        |> Floki.find(".row .col-md-7")
+        |> Stream.map(&parse_article/1)
 
+      {:ok, articles}
+    else
+      {_, response} -> {:error, response}
     end
+  end
 
+  def article_details(%{url: url} = article) do
+    response = ScrapingHelper.get(url)
+
+    with {:ok, %{status_code: 200, body: body}} <- response,
+         {:ok, html} <- Floki.parse_document(body) do
+      article_container =
+        html
+        |> Floki.find(".container--item")
+
+      case ScrapingHelper.get_og_description(html) do
+        {:ok, description} ->
+          {:ok, Map.put(article, :description, description)}
+
+        {:error, _} ->
+          {:error, :not_available}
+      end
+
+      date =
+        article_container
+        |> Floki.find(".card__category-time span ")
+        |> Enum.at(0)
+        |> Floki.text()
+        |> String.trim()
+
+      published_at =
+        date
+        |> Timex.parse!("{D}/{M}/{YYYY} u {h24}:{m} h")
+        |> DateTime.from_naive!("Europe/Sarajevo")
+        |> DateTime.shift_zone!("Etc/UTC")
+        |> DateTime.to_naive()
+
+      Map.put(article, :published_at, published_at)
+
+      case ScrapingHelper.get_og_image(html) do
+        {:ok, image_url} ->
+          {:ok, Map.put(article, :image, image_url)}
+
+        {:error, _} ->
+          {:error, :image_not_available}
+      end
+    else
+      _ -> {:error, :article_not_available}
+    end
+  end
+
+  def parse_article(article) do
+    url =
+      article
+      |> Floki.find("a")
+      |> Floki.attribute("href")
+      |> Enum.at(0)
+
+    path =
+      url
+      |> String.split("/")
+      |> Enum.fetch!(2)
+
+    original_id =
+      path
+      |> String.split("-")
+      |> Enum.at(9)
+
+    title =
+      article
+      |> Floki.find("a")
+      |> Floki.attribute("title")
+      |> Enum.at(0)
+
+    %{
+      original_id: original_id,
+      title: title,
+      description: nil,
+      published_at: nil,
+      author: nil,
+      image: nil,
+      url: URI.encode("https://bhdani.oslobodjenje.ba/" <> url)
+    }
   end
 end
--- test/prati_ba/scrapers/bh_dani_test.exs
+++ test/prati_ba/scrapers/bh_dani_test.exs
@@ -13,10 +13,73 @@ defmodule PratiBa.Scrapers.BhDaniTest do
       Bypass.expect(bypass, fn conn ->
         Plug.Conn.resp(conn, 200, articles_payload())
       end)
+
+      response = BhDaniScraper.articles("http://localhost:#{bypass.port}/")
+
+      assert {:ok, articles} = response
+
+      articles = Enum.to_list(articles)
+
+      assert length(articles) == 1
+
+      assert %{
+               original_id: "778048",
+               title: "Na kontinentu koji se nekad zvao Evropa: Mutantova priča",
+               description: nil,
+               published_at: nil,
+               author: nil,
+               image: nil,
+               url:
+                 "https://bhdani.oslobodjenje.ba//bhdani/na-kontinentu-koji-se-nekad-zvao-evropa-mutantova-prica-778048"
+             } = Enum.at(articles, 0)
+    end
   end
 
-  def articles_payload do
+  describe "article_details/1" do
+    test "fetches article image", %{bypass: bypass} do
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/na-kontinentu-koji-se-nekad-zvao-evropa-mutantova-prica-778048",
+        fn conn ->
+          Plug.Conn.resp(conn, 200, article_payload())
+        end
+      )
 
+      article_url =
+        "http://localhost:#{bypass.port}/na-kontinentu-koji-se-nekad-zvao-evropa-mutantova-prica-778048"
+
+      article = %{
+        original_id: "778048",
+        title: "Na kontinentu koji se nekad zvao Evropa: Mutantova priča",
+        description: nil,
+        published_at: ~N[2022-08-30 07:17:00],
+        author: nil,
+        image: nil,
+        url: article_url
+      }
+
+      response = BhDaniScraper.article_details(article)
+
+      assert {:ok, article} = response
+
+      assert %{
+               original_id: "778048",
+               title: "Na kontinentu koji se nekad zvao Evropa: Mutantova priča",
+               description: nil,
+               published_at: ~N[2022-08-30 07:17:00],
+               author: nil,
+               image: "https://cdn.oslobodjenje.ba/images/slike/new/2022/07/22/6276563.jpg",
+               url: ^article_url
+             } = article
+    end
   end
 
+  defp articles_payload do
+    File.read!("test/support/payloads/bh_dani_scraper/articles.html")
+  end
+
+  defp article_payload do
+    File.read!("test/support/payloads/bh_dani_scraper/article.html")
+  end
 end
