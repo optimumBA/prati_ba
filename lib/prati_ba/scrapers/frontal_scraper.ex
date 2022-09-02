44dbdf95cfc74a7303434fb29950614a75c09146
senaid123 <senaid968@gmail.com>
2022-09-02 14:48:19 +0200

    Implemented scraper for Frontal.ba article -add scraper for Frontal.ba -write tests for scraper functionality -add new scrapr to scrapers -add scraper to seeds

---
+++ lib/prati_ba/scrapers/frontal_scraper.ex
@@ -0,0 +1,63 @@
+defmodule PratiBa.Scrapers.FrontalScraper do
+  @behaviour PratiBa.Scrapers.Scraper
+
+  @rss_url "https://www.frontal.ba/rss"
+
+  alias PratiBa.Scrapers.ScrapingHelper
+
+  def articles(url \\ @rss_url) do
+    response = ScrapingHelper.get(url)
+
+    case response do
+      {:ok, %{status_code: 200, body: body}} ->
+        {:ok, rss} =
+          body
+          |> HtmlEntities.decode()
+          |> FastRSS.parse()
+
+        articles =
+          rss["items"]
+          |> Stream.map(&parse_article/1)
+
+        {:ok, articles}
+
+      {_, response} ->
+        {:error, response}
+    end
+  end
+
+  def parse_article(article) do
+    %{
+      "guid" => %{
+        "value" => url
+      },
+      "pub_date" => date,
+      "title" => title,
+      "description" => description,
+      "enclosure" => %{
+        "url" => image_url
+      }
+    } = article
+
+    original_id =
+      url
+      |> String.split("/")
+      |> Enum.fetch!(4)
+
+    published_at =
+      date
+      |> Timex.parse!("{RFC1123}")
+      |> DateTime.shift_zone!("Etc/UTC")
+      |> DateTime.to_naive()
+
+    %{
+      original_id: original_id,
+      title: title,
+      description: description,
+      published_at: published_at,
+      author: nil,
+      image: image_url,
+      url: URI.encode(url)
+    }
+  end
+end
