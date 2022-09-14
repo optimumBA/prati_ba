d17808614d33d40b1212980f641036609ddcaebd
senaid123 <senaid968@gmail.com>
2022-09-15 01:58:14 +0200

    remove should_scrape/1

--- lib/prati_ba/scrapers/n1_scraper.ex
+++ lib/prati_ba/scrapers/n1_scraper.ex
@@ -35,7 +35,6 @@ defmodule PratiBa.Scrapers.N1Scraper do
   def article_details(article), do: {:ok, article}
 
   defp should_scrape(%{link: "https://ba.n1info.com/english/" <> _}), do: false
-  defp should_scrape(_), do: true
 
   defp parse_article(article) do
     %{
