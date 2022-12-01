ff97315f3823f6ab6ebd601d11a053c5dd95a7a2
senaid123 <senaid968@gmail.com>
2022-12-01 18:43:57 +0100

    try to fix error with newer version of phx_live_reload

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule PratiBa.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.4", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
       {:floki, ">= 0.30.0"},
       {:phoenix_live_dashboard, "~> 0.7.2"},
