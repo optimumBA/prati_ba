f981611ca3f0fafa0db2a96235ff291f93c10265
Nittin Shankar <nittin.shankar@gmail.com>
2023-03-17 11:06:16 +0530

    Load article images by allowing it in static paths

--- lib/prati_ba_web.ex
+++ lib/prati_ba_web.ex
@@ -17,7 +17,7 @@ defmodule PratiBaWeb do
   and import those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(articles assets fonts images favicon.ico robots.txt)
 
   def verified_routes do
     quote do
