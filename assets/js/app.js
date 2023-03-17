b3b9c0462344dd5adb651f3f5cd29493c3589913
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-17 22:09:29 +0100

    Add js hook for resizing the header frame when scrolling down

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,12 +25,14 @@ import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import topbar from 'topbar'
 import DisplayTimeHook from './hooks/display_time_hook'
+import HeaderHook from './hooks/header_hook'
 import InfiniteScrollHook from './hooks/infinite_scroll_hook'
 import ScrollDownHook from './hooks/scroll_down_hook'
 import ToggleBgHook from './hooks/toggle_bg_hook'
 
 Hooks = {
   DisplayTimeHook,
+  HeaderHook,
   InfiniteScrollHook,
   ScrollDownHook,
   ToggleBgHook
