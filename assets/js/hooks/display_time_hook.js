d2bba77373b32102792ddd6402ea712bd2b4ff74
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-20 08:51:20 +0100

    Format JS code

--- assets/js/hooks/display_time_hook.js
+++ assets/js/hooks/display_time_hook.js
@@ -1,17 +1,17 @@
 export default {
-    mounted() {
-        let currentTimeElement = this.el.querySelector('#current_time')
+  mounted() {
+    let currentTimeElement = this.el.querySelector('#current_time')
 
-        currentTimeElement.innerHTML = getCurrentTime()
+    currentTimeElement.innerHTML = getCurrentTime()
 
-        setInterval(() => {
-            currentTimeElement.innerHTML = getCurrentTime()
-        }, 1000)
+    setInterval(() => {
+      currentTimeElement.innerHTML = getCurrentTime()
+    }, 1000)
 
-        function getCurrentTime() {
-            let date = new Date()
-            let current_time = date.toTimeString().slice(0, 5)
-            return current_time
-        }
-    },
+    function getCurrentTime() {
+      let date = new Date()
+      let current_time = date.toTimeString().slice(0, 5)
+      return current_time
+    }
+  },
 }
--- assets/js/hooks/header_hook.js
+++ assets/js/hooks/header_hook.js
@@ -1,22 +1,21 @@
 export default {
-    mounted() {
+  mounted() {
+    let header = this.el
+    let appLogo = header.querySelector('#app-logo')
+    let appLogoDark = header.querySelector('#app-logo-dark')
 
-        let header = this.el
-        let appLogo = header.querySelector('#app-logo')
-        let appLogoDark = header.querySelector('#app-logo-dark')
+    const headerPos = header.getBoundingClientRect()
 
-        const headerPos = header.getBoundingClientRect();
+    document.addEventListener('scroll', (event) => {
+      const currentScroll = window.pageYOffset
 
-        document.addEventListener("scroll", (event) => {
-            const currentScroll = window.pageYOffset;
-
-            if (currentScroll > headerPos.bottom) {
-                appLogo.classList.add('sm:w-24')
-                appLogoDark.classList.add('sm:w-24')
-            } else {
-                appLogo.classList.remove('sm:w-24')
-                appLogoDark.classList.remove('sm:w-24')
-            }
-        });
-    },
+      if (currentScroll > headerPos.bottom) {
+        appLogo.classList.add('sm:w-24')
+        appLogoDark.classList.add('sm:w-24')
+      } else {
+        appLogo.classList.remove('sm:w-24')
+        appLogoDark.classList.remove('sm:w-24')
+      }
+    })
+  },
 }
