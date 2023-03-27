c6bbc495b2b0ea5a48d8cc936f970c0e67cb7324
Nittin Shankar <nittin.shankar@gmail.com>
2023-03-27 16:12:23 +0530

    Fix the root layout of default page

--- lib/prati_ba_web/router.ex
+++ lib/prati_ba_web/router.ex
@@ -7,7 +7,7 @@ defmodule PratiBaWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, {PratiBaWeb.Layouts, :root}
+    plug :put_root_layout, html: {PratiBaWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
