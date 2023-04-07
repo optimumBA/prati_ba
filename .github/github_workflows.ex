4e547f0163ff90d4cc0c91e3d514175a2dc62698
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-04-07 10:20:09 +0200

    Try again

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -83,7 +83,7 @@ defmodule GitHubWorkflows do
             secrets:
               "ADMIN_PASSWORD=${{ secrets.ADMIN_PASSWORD }} MAXMIND_LICENSE_KEY=${{ secrets.MAXMIND_LICENSE_KEY }}",
             setup_cmd:
-              "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source '$HOME/.cargo/env'"
+              "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source \"$HOME/.cargo/env\""
           ]
         ]
       ]
