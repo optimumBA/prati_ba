eb5921f721fe9c2061681ae11b8ab707eed40936
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-04-11 11:44:01 +0200

    Add missing app removal step

--- .github/github_workflows.ex
+++ .github/github_workflows.ex
@@ -20,12 +20,56 @@ defmodule GitHubWorkflows do
           ],
           pull_request: [
             branches: ["main"],
-            types: ["opened", "reopened", "synchronize"]
+            types: ["closed", "opened", "reopened", "synchronize"]
           ]
         ],
         jobs: [
+          delete_preview_app: delete_preview_app_job(),
           deploy_preview_app: deploy_preview_app_job(),
           test: test_job()
+        ]
+      ]
+    ]
+  end
+
+  defp delete_preview_app_job do
+    [
+      name: "Delete preview app",
+      if: "github.event_name == 'pull_request' && github.event.type == 'closed'",
+      "runs-on": "ubuntu-latest",
+      concurrency: [group: "pr-${{ github.event.number }}"],
+      env: [
+        FLY_API_TOKEN: "${{ secrets.FLY_API_TOKEN }}",
+        REPO_NAME: "prati_ba"
+      ],
+      steps: [
+        [
+          uses: "actions/checkout@v2"
+        ],
+        [
+          name: "Delete preview app",
+          uses: "almirsarajcic/fly-pr-review-apps@vm-size",
+          with: [
+            name: "pr-${{ github.event.number }}-prati-ba"
+          ]
+        ],
+        [
+          name: "Generate token",
+          uses: "navikt/github-app-token-generator@v1.1.1",
+          id: "generate_token",
+          with: [
+            "app-id": "${{ secrets.GH_APP_ID }}",
+            "private-key": "${{ secrets.GH_APP_PRIVATE_KEY }}"
+          ]
+        ],
+        [
+          name: "Delete GitHub environment",
+          uses: "strumwolf/delete-deployment-environment@v2.2.3",
+          with: [
+            token: "${{ ssteps.generate_token.outputs.token  }}",
+            environment: "pr-${{ github.event.number }}-prati-ba",
+            ref: "${{ github.head_ref }}"
+          ]
         ]
       ]
     ]
@@ -34,7 +78,7 @@ defmodule GitHubWorkflows do
   defp deploy_preview_app_job do
     [
       name: "Deploy preview app",
-      if: "github.event_name == 'pull_request'",
+      if: "github.event_name == 'pull_request' && github.event.type != 'closed'",
       needs: ["test"],
       permissions: "write-all",
       "runs-on": "ubuntu-latest",
@@ -81,6 +125,7 @@ defmodule GitHubWorkflows do
   defp test_job do
     [
       name: "Test",
+      if: "github.event.type != 'closed'",
       "runs-on": "ubuntu-latest",
       services: [
         db: [
