e605737f02e763ef4075cad46fb4017465a6ee40
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-12-18 12:41:52 +0100

    Fix volume mounting issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 bin/prati_ba eval 'UAInspector.Downloader.download'
-ln -s /data/articles lib/prati_ba-0.1.0/priv/static/articles
+
+rm -rf /app/lib/prati_ba-0.1.0/priv/static/articles
+mkdir -p /data/articles
+ln -s /data/articles/ /app/lib/prati_ba-0.1.0/priv/static/articles
 
 exec "$@"
